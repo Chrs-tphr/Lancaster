fbeb1d7f3570ce06d15c209252deb395df1c8638
Christopher <cisensano@yahoo.com>
2016-08-24 00:33:43 -0400

    Added additional task and status (Application Intake, Accepted)

--- EVENTS/WTUA;Licenses!~!~!Application.js
+++ EVENTS/WTUA;Licenses!~!~!Application.js
@@ -9,8 +9,9 @@
 // Developer Agency: Woolpert
 // Script Description: When a status of Issued is applied to workflow tasks License Issuance; then check balance for all assessed and invoiced fees. If balance is greater zero then do not allow the status of Issued to be submitted.
 // Status: Active
+// Notes: 8/23/2016 - Added additional task and status (Application Intake, Accepted)
 
-010 - (wfTask == "License Issuance" && wfStatus == "Issued" && (balanceDue > 0 || feeTotalByStatus("NEW") > 0)) ^ showMessage = true; comment("The record has an outstanding balance which must be paid before it can be issued or fees that have not been invoiced."); cancel = true;
+010 - (matches(wfTask, "Application Intake", "License Issuance") && matches(wfTask, "Accepted", "Issued") && (balanceDue > 0 || feeTotalByStatus("NEW") > 0)) ^ showMessage = true; comment("The record has an outstanding balance which must be paid before it can be issued or fees that have not been invoiced."); cancel = true;
 
 ===================================================================*/
 
