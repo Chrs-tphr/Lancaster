c0e271736c78d44c617e58d92fd2b7288bc68dc9
Christopher <cisensano@yahoo.com>
2016-08-26 15:19:12 -0400

    Removed fee balance script

--- EVENTS/WTUA;Licenses!~!~!Renewal.js
+++ EVENTS/WTUA;Licenses!~!~!Renewal.js
@@ -3,14 +3,14 @@
 ===================================================================*/
 
 /*===================================================================
-// ID: 33
-// Name: Zero Balance Fee Check
+// ID: 
+// Name: 
 // Developer: Chris Godwin
 // Developer Agency: Woolpert
-// Script Description: When a status of Renewed is applied to workflow task License Issuance then check balance for all assessed and invoiced fees. If balance is greater zero then do not allow the wf task status of Renewed to be submitted.
+// Script Description: 
 // Status: Active
-// Notes: 8/23/2016 - Added additional task and status (Renewal Intake, Fees Paid)
+// Notes: 
 
-010 - (matches(wfTask, "License Issuance", "Renewal Intake") && matches(wfTask, "Renewed", "Fees Paid") && (balanceDue > 0 || feeTotalByStatus("NEW") > 0)) ^ showMessage = true; comment("The record has an outstanding balance which must be paid before it can be issued or fees that have not been invoiced."); cancel = true;
+010 - 
 
 ===================================================================*/
