0bbf8c644597dc2c5aa880ed49b36482273a205c
Christopher <cisensano@yahoo.com>
2016-09-13 23:20:23 -0700

    updated multiple functions

--- Custom Scripts/INCLUDES_CUSTOM_LIC.js
+++ Custom Scripts/INCLUDES_CUSTOM_LIC.js
@@ -9,23 +9,18 @@
 |         
 /------------------------------------------------------------------------------------------------------*/
 
+function calcMonthsLate(date1){//date1 is the ASI Business Open Date (Application) or License Expiration Date (Renewal) passed by calculateLicAppRenewPenaltyFee
+//		var fDate =  new Date("12/01/2016");
 
-
-
-function monthsLate(){
-//	var oDate = new Date("01/01/2016");
-//	var fDate =  new Date("12/01/2016");
-
-	var oDate = convertDate(AInfo["Business Open Date"]); logDebug("oDate: "+oDate);
 	var fDate = convertDate(fileDate); logDebug("fDate: "+fDate);
 	var monthLate = 0;
 	var yMonthLate = 0;
 	var monthsLate = 0;
 	
-	if(oDate < fDate){
+	if(date1 < fDate){
 		
-		var oMonth = oDate.getMonth();
-		var oYear = oDate.getFullYear();
+		var oMonth = date1.getMonth();
+		var oYear = date1.getFullYear();
 			logDebug("oMonth: "+oMonth+" oYear: "+oYear);
 		
 		var fDateMonth = fDate.getMonth();
@@ -33,34 +28,57 @@ function monthsLate(){
 			logDebug("fDateMonth: "+fDateMonth+" fDateYear: "+fDateYear);
 		
 		if((oYear < fDateYear) || (oYear == fDateYear && oMonth < fDateMonth)){
-			monthLate = (fDateMonth - oMonth); logDebug(monthLate);
-			yMonthLate = ((fDateYear - oYear)*12); logDebug(yMonthLate);
+			monthLate = (fDateMonth - oMonth); logDebug("Months late: "+ monthLate);
+			yMonthLate = ((fDateYear - oYear)*12); logDebug("Years late: "+ yMonthLate);
 			monthsLate = monthLate+yMonthLate;
 		}
-		logDebug(monthsLate);
+		logDebug("Total months late: "+ monthsLate);
 		return monthsLate;
 	}
 }
 
-function calculateLicAppPenaltyFee(){
-	var appFee = 0;
-	var monthsLate = monthsLate();
-	var penaltyPercent = 0;
-	var pAmount = 0;
-	if(monthsLate == 1) penaltyPercent = 0.2;
-	if(monthsLate == 2) penaltyPercent = 0.3;
-	if(monthsLate == 3) penaltyPercent = 0.4;
-	if(monthsLate > 3) penaltyPercent = 0.5;
-	
-	if(matches(appType[2], "Group Home")){
-		appFee = feeAmount("S-084N");
-		pAmount = appFee*penaltyPercent;
-		updateFee("S-084P","LIC_GH_GENERAL","FINAL","pAmount","Y");
-	}else{
-		appFee = feeAmount("LIC_020");
-		pAmount = appFee*penaltyPercent;
-		updateFee("LIC_050","LIC_BUSINESS_GENERAL","FINAL","pAmount","Y");//LIC_050 is configured, need to confirm configuration with client.
+function calculateLicAppRenewPenaltyFee(){
+	var date1 = null;
+	if(appTypeArray[3] == "Application"){
+		var recType = "app";
+		var date1 = convertDate(AInfo["Business Open Date"]);
+		logDebug("Business Open Date: "+date1);
 	}
+	if(appTypeArray[3] == "Renewal"){
+		var recType = "rnew";
+		var parentLicenseCAPID = getParentLicenseCapID(capId);
+		if (parentLicenseCAPID != null){
+			var date1 = parentLicenseCAPID.getExpDate();
+			logDebug("License Expiration Date: "+date1);
+		}else{
+			logDebug("No parent license found");
+		}
+	}
+	if(date1){
+		var monthsLate = calcMonthsLate(date1);
+		var pPercent = 0;
+		var pAmount = 0;
+		if(monthsLate == 1) pPercent = 0.2;//20%
+		if(monthsLate == 2) pPercent = 0.3;//30%
+		if(monthsLate == 3) pPercent = 0.4;//40%
+		if(monthsLate > 3 && monthsLate < 13) pPercent = 0.5;//50%
+		if(monthsLate > 12 && monthsLate < 25) pPercent = 1.0;//100%
+		if(monthsLate > 24) pPercent = 1.5;//150%
+		logDebug("pPercent:" + pPercent);
+		if(pPercent > 0){
+			addFee("BLPN050","BL_PENALTY","FINAL",1,"N");//adds SG fee to get total fee amount to be penalized
+			var lFee = feeAmount("BLPN050", "NEW");
+			pAmount = lFee*pPercent;
+			removeFee("BLPN050", "FINAL");//removes SG fee after penalty amount is calculated
+			if(recType == "app"){
+				updateFee("BLPN060","BL_PENALTY","FINAL",pAmount,"N");
+			}
+			if(recType == "rnew"){
+				updateFee("BLPN060","BL_PENALTY","FINAL",pAmount,"N");
+			}
+		}
+	}
+	else logDebug("date1 was not set");
 }
 
 function daysBetween(date1, date2){
@@ -96,32 +114,6 @@ function getParentLicenseCapID(capid) {
 		return projectScriptModel.getProjectID();
 	} else {
 		return getParentCapVIAPartialCap(capid);
-	}
-}
-
-function calculateLicRenewalPenaltyFee(){
-	var parentLicenseCAPID = getParentLicenseCapID(capID)
-	if (parentLicenseCAPID != null){
-		var rnewFee = 0;
-		var date1 = parentLicenseCAPID.getExpDate();
-		var date2 = fileDate;
-		var daysLate = daysBetween(date1, date2);
-		var penaltyPercent = 0;
-		var pAmount = 0;
-		if(daysLate == 1) penaltyPercent = 0.2;
-		if(daysLate == 2) penaltyPercent = 0.3;
-		if(daysLate == 3) penaltyPercent = 0.4;
-		if(daysLate > 3) penaltyPercent = 0.5;
-		
-		if(matches(appType[2], "Group Home")){
-			appFee = feeAmount("S-084N");
-			pAmount = appFee*penaltyPercent;
-			updateFee("S-084P","LIC_GH_GENERAL","FINAL","pAmount","Y");
-		}else{
-			appFee = feeAmount("LIC_020");
-			pAmount = appFee*penaltyPercent;
-			updateFee("LIC_050","LIC_BUSINESS_GENERAL","FINAL","pAmount","Y");//LIC_050 needs to be configured
-		}
 	}
 }
 
